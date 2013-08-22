5e499771023db6e4729b509960da5e500f74339c
loopasam <samuel.croset@gmail.com>
2013-08-22 17:12:44 +0100

    breadcrumbs implemented

--- svn/trunk/content/js/breadcrumbs.js
+++ svn/trunk/content/js/breadcrumbs.js
@@ -1,20 +1,40 @@
 $(document).ready(function() {
-	var url = $(location).attr('href');
 
-	//Regex must be modified by export website script!
-	var matches = url.split('/').slice(3);
-	console.log(matches);
-	var breadcrumbs = '<ol class="breadcrumb">';
-	matches.forEach(function(entry) {
-		console.log("entry: " + entry);
-		breadcrumbs += '<li><a href="#">' + entry + '</a></li>'; 
-	});
-	breadcrumbs += '</ol>';
-	$('#breadcrumbs').append(breadcrumbs);
+	var url = $(location).attr('href');
+	var prefix = 'http://localhost/';
+
+	if(url != prefix && url != prefix + 'index.html'){
+
+		var shortForm = url.replace(prefix, '').replace(/(index)?\.html/g, '').replace(/#.*/g, '');
+	
+		var crumbs = shortForm.split('/');
+
+		if(crumbs[crumbs.length-1] == ""){
+			crumbs.pop();
+		}
+
+		var link = prefix;
+	
+		var breadcrumbs = '<ol class="breadcrumb">';
+	
+		for (var index = 0; index < crumbs.length; ++index) {
+			var crumb = crumbs[index];
+			link += crumb + '/';
+
+			//Check if it is the last element of the array
+			if(crumbs.length - 1 == index){
+				//If yes then declare as active
+				breadcrumbs += '<li class="active">' + crumb.toUpperCase().replace(/_|-/g, ' ') + '</li>'; 
+			}else{
+				//if not then print a link for the category
+				breadcrumbs += '<li><a href="' + link + '">' + crumb.toUpperCase().replace(/_|-/g, ' ') + '</a></li>'; 
+			}
+		}
+		breadcrumbs += '</ol>';
+		$('#breadcrumbs').append(breadcrumbs);
+	}else{
+		$('#breadcrumbs').hide();
+	}
 });
 
-//TODO
-//Handle urls to link parts
-// get rid of index.html and the html trailing if not index
-// Get rid of the underscore in labels
-//Get read of breadcrumbs on the home page
+//update exporter to handle gh-pages
